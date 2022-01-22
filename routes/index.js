b95fe1648cfe56dcc0134df44c25a0dc1257e346
Rushali Patnaik <rpatnaik78@gmail.com>
2022-01-22 10:16:55 +0000

    header added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.landing);
-
-
 module.exports = router
